834814d7b52cbe204cf0f53af4128fc974b54405
liuqi <liuqi6602@163.com>
2023-12-08 14:38:17 +0800

    save

--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -1,21 +1,21 @@
 span
 <template>
-    <sister-header />
-    <full-page>
-        <full-page-item flex justify-center>
-            <div class="lg:w-85vw lt-lg:w-90vw mt-1em trans">
-                <sister-timeline />
-            </div>
-        </full-page-item>
-    </full-page>
-    <sister-footer />
+  <sister-header />
+  <full-page>
+    <full-page-item flex justify-center>
+      <div class="lg:w-85vw lt-lg:w-90vw mt-1em trans">
+        <sister-timeline />
+      </div>
+    </full-page-item>
+  </full-page>
+  <sister-footer />
 </template>
 
 <script setup lang="ts">
 const seo = {
-    title: '妹妹的记事本',
-    description: 'seo描述',
-    keywords: 'seo关键字',
+  title: '妹妹的记事本',
+  description: 'seo描述',
+  keywords: '妹妹的记事本，记事本，日记',
 }
 
 useSeoMeta(seo)
@@ -23,8 +23,8 @@ useSeoMeta(seo)
 
 <style lang="scss" scoped>
 @media screen and (max-width: 576px) {
-    .trans {
-        transform: translateX(-5%);
-    }
+  .trans {
+    transform: translateX(-5%);
+  }
 }
 </style>
